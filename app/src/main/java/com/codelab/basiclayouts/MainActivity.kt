ad388885dc8dc40ffdf6d938759a082c96f58e4f
Utkarsh Karki <utkarsh1998@icloud.com>
2024-03-28 16:21:08 -0700

    Added the search icon, deafult from Material

--- app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.codelab.basiclayouts
 
 import android.os.Bundle
@@ -21,7 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,7 +30,17 @@ class MainActivity : ComponentActivity() {
 fun SearchBar(
     modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+                      Icon(Icons.Default.Search, contentDescription = null)
+        },
+        modifier = modifier
+            // making sure that if the user's font size is larger, our search bar expands accordingly
+            .heightIn(min = 56.dp)
+            .fillMaxWidth()
+    )
 }
 
 // Step: Align your body - Alignment
@@ -116,7 +116,7 @@ fun MySootheAppLandscape(){
 // Step: MySoothe App
 @Composable
 fun MySootheApp() {
-    // Implement composable here
+
 }
 
 private val alignYourBodyData = listOf(
